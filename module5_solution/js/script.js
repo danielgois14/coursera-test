d1c27590ff63a1bf98acc3574dbc64eeed326e6d
danielgois14 <danielgois14@gmail.com>
2021-06-23 12:03:56 -0300

    working

--- module5_solution/js/script.js
+++ module5_solution/js/script.js
@@ -83,7 +83,7 @@ $(function () {
       allCategoriesUrl,
       buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
       true
-    ); // Explicitely setting the flag to get JSON from server processed into an object literal
+    ); // Explicitly setting the flag to get JSON from server processed into an object literal
   });
   // *** finish **
 
@@ -97,7 +97,6 @@ $(function () {
         // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
         // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
         // variable's name implies it expects.
-        // var chosenCategoryShortName = ....
         var chosenCategoryShortName =
           chooseRandomCategory(categories).short_name;
 
@@ -105,14 +104,13 @@ $(function () {
         // chosen category from STEP 2. Use existing insertProperty function for that purpose.
         // Look through this code for an example of how to do use the insertProperty function.
         // WARNING! You are inserting something that will have to result in a valid Javascript
-        // syntax because the substitution of {{randomCategoryShortName}} becomes an argument
+        // syntax because the substitution of {{insertProperty}} becomes an argument
         // being passed into the $dc.loadMenuItems function. Think about what that argument needs
         // to look like. For example, a valid call would look something like this:
         // $dc.loadMenuItems('L')
         // Hint: you need to surround the chosen category short name with something before inserting
         // it into the home html snippet.
         //
-        // var homeHtmlToInsertIntoMainPage = ....
         chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
         var homeHtmlToInsertIntoMainPage = insertProperty(
           homeHtml,
@@ -123,7 +121,6 @@ $(function () {
         // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
         // Use the existing insertHtml function for that purpose. Look through this code for an example
         // of how to do that.
-        // ....
         insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
       },
       false
